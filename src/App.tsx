2f75126257b97526816dbc9cf4aa13e34023074c
designyamah <nnyamahjoseph@gmail.com  git config --global user.name designyamahgit config --global user.email nnyamahjoseph@gmail.com>
2025-04-24 15:39:31 +0100

    Setup project structure and install dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+
 function App() {
   return (
-    <>
-      <h1>Happy Coding</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
